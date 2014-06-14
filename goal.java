cd52a6ccb65609cb53403d4a4317d4dc0f9c1d9d
Aleksey <alexshtanko23@yandex.ua>
2014-06-14 22:01:04 +0300

    Create goal.java

---
+++ goal.java
@@ -0,0 +1,9 @@
+public class Goal {
+    public static void main(String[] args) {
+        goal("Learn Android Development");
+    }
+
+    public static void goal(String goal) {
+        System.out.println(goal);
+    }
+}
